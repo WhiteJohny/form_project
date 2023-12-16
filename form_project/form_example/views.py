e6b8d942bc2485394ce86d4e1137bb72c0eb768b
WhiteJohny <vvvfrog2004@gmail.com>
2023-12-17 00:29:14 +0300

    form class

--- form_project/form_example/views.py
+++ form_project/form_example/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .forms import ExampleForm
 
 
 def form_example(request):
-    for name in request.POST:
-        print("{}: {}".format(name, request.POST.getlist(name)))
-    return render(request, 'form-example.html', {"method": request.method})
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            for name, value in form.cleaned_data.items():
+                print("{}: ({}) {}".format(name, type(value), value))
+    else:
+        form = ExampleForm()
+
+    return render(request, 'form-example.html', {"method": request.method, "form": form})
